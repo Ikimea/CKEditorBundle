7c01d5239035f810bbd4f7a920519978d4ab5450
mmbechezi <mlanawo.mbechezi@ikimea.com>
2011-08-26 23:53:23 +0200

    Add architecture

---
+++ CKEditorBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+* This file is part of CKeditortBundle
+* (c) 2011 Mbechezi Mlanawo
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+
+namespace Ikimea\CKeditortBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CKeditortBundle extends Bundle{
+    
+}
