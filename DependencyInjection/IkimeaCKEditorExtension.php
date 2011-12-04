c6b43e23923bf3d83a70994ce0caa71fcf22bc9b
Mlanawo Mbechezi <mlanawo.mbechezi@ikimea.com>
2011-12-04 21:31:49 +0100

    Updagrade vendor

--- DependencyInjection/IkimeaCKEditorExtension.php
+++ DependencyInjection/IkimeaCKEditorExtension.php
@@ -12,23 +12,26 @@ namespace Ikimea\CKEditorBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-
+use Symfony\Component\Config\FileLocator;
 
 
 class IkimeaCKEditorExtension extends Extension {
 
     public function load(array $configs, ContainerBuilder $container) {
-        //$loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        //$loader->load('services.yml');
-        
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        $container->setParameter('twig.form.resources', array_merge(
+            $container->getParameter('twig.form.resources'), array('IkimeaCKEditorBundle:Form:ckeditor_widget.html.twig')
+        ));
+
+
+
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
     }
-    
-    public function getAlias(){
+
+    public function getAlias() {
         return 'ikimea_ck_editor';
     }
 
