b0bb8b0dccb5ec04e183d0ab1ab02a21becc73d1
mmbechezi <mlanawo.mbechezi@ikimea.com>
2012-07-26 21:33:28 +0300

    Update Form/Type/CKEditorType.php

--- Form/Type/CKEditorType.php
+++ Form/Type/CKEditorType.php
@@ -29,7 +29,7 @@ class CKEditorType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function buildView(FormViewInterface $view, FormInterface $form, array $options)
+    public function buildView(FormView $view, FormInterface $form, array $options)
     {
         $view
             ->setVar('toolbar', $form->getAttribute('toolbar')
