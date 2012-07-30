0aaae4b2aed3066e70c1a1c1cf31cdad72e4a57d
mmbechezi <mlanawo.mbechezi@ikimea.com>
2012-07-30 17:07:41 +0200

    Update Form/Type/CKEditorType.php

--- Form/Type/CKEditorType.php
+++ Form/Type/CKEditorType.php
@@ -4,7 +4,7 @@ namespace Ikimea\CKEditorBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormViewInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
